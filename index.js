ed3023070a11c7030d2fda24431fbbb39e8e1c33
Dave Strus <dave@getfretless.com>
2018-06-05 14:57:30 -0400

    Update heading with text from the form

--- index.js
+++ index.js
@@ -3,9 +3,12 @@ const form = document.querySelector('form')
 const changeHeading = function(ev) {
   ev.preventDefault()
 
+  const f = ev.target
+  const spellName = f.spellName.value
+
   document
     .querySelector('h1')
-    .textContent = 'Ye Olde Spellbook'
+    .textContent = spellName
 }
 
 form.addEventListener('submit', changeHeading)
