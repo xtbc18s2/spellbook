a9b099d040e51a01b12d330a8753913d5da5fa20
Dave Strus <dave@getfretless.com>
2018-06-05 09:37:02 -0400

    Update the heading when the button is clicked

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log('testing')
+const button = document.querySelector('button')
+
+const changeHeading = function() {
+  document
+    .querySelector('h1')
+    .textContent = 'Ye Olde Spellbook'
+}
+
+button.addEventListener('click', changeHeading)
