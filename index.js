9eaaa44393e962d751f23985a57a228f9c8fbdea
Dave Strus <dave@getfretless.com>
2018-06-06 16:30:32 -0400

    Organize code in an object and use arrow functions

--- index.js
+++ index.js
@@ -1,49 +1,55 @@
-const form = document.querySelector('form')
+const app = {
+  init: function() {
+    const form = document.querySelector('form')
+    form.addEventListener('submit', (ev) => {
+      ev.preventDefault()
+      this.handleSubmit(ev)
+    })
+  },
 
-const renderProperty = function(name, value) {
-  const el = document.createElement('span')
-  el.classList.add(name)
-  el.textContent = value
-  el.setAttribute('title', value)
-  return el
+  renderProperty: function(name, value) {
+    const el = document.createElement('span')
+    el.classList.add(name)
+    el.textContent = value
+    el.setAttribute('title', value)
+    return el
+  },
+
+  renderItem: function(spell) {
+    // ['name', 'level']
+    const properties = Object.keys(spell)
+
+    // collect an array of <span> elements
+    const childElements = properties.map((prop) => {
+      return this.renderProperty(prop, spell[prop])
+    })
+
+    const item = document.createElement('li')
+    item.classList.add('spell')
+
+    // append each <span> to the <li>
+    childElements.forEach(function(el) {
+      item.appendChild(el)
+    })
+
+    return item
+  },
+
+  handleSubmit: function(ev) {
+    const f = ev.target
+
+    const spell = {
+      name: f.spellName.value,
+      level: f.level.value,
+    }
+
+    const item = this.renderItem(spell)
+
+    const list = document.querySelector('#spells')
+    list.appendChild(item)
+
+    f.reset()
+  },
 }
 
-const renderItem = function(spell) {
-  // ['name', 'level']
-  const properties = Object.keys(spell)
-
-  // collect an array of <span> elements
-  const childElements = properties.map(function(prop) {
-    return renderProperty(prop, spell[prop])
-  })
-
-  const item = document.createElement('li')
-  item.classList.add('spell')
-
-  // append each <span> to the <li>
-  childElements.forEach(function(el) {
-    item.appendChild(el)
-  })
-
-  return item
-}
-
-const handleSubmit = function(ev) {
-  ev.preventDefault()
-
-  const f = ev.target
-
-  const spell = {
-    name: f.spellName.value,
-    level: f.level.value,
-  }
-
-  const item = renderItem(spell)
-
-  const list = document.querySelector('#spells')
-  list.appendChild(item)
-
-  f.reset()
-}
-
-form.addEventListener('submit', handleSubmit)
+app.init()
