56aef0ca93834a012b462e2ce34b9eafb10e7d96
Dave Strus <dave@getfretless.com>
2018-06-06 11:40:02 -0400

    Extract renderItem function (using map() and forEach())

--- index.js
+++ index.js
@@ -7,23 +7,40 @@ const renderProperty = function(name, value) {
   return el
 }
 
+const renderItem = function(spell) {
+  // ['name', 'level']
+  properties = Object.keys(spell)
+
+  // collect an array of renderProperty's return values
+  // (an array of <span> elements)
+  const childElements = properties.map(function(property) {
+    return renderProperty(property, spell[property])
+  })
+
+  const item = document.createElement('li')
+  item.classList.add('spell')
+
+  // append each <span> to the <li>
+  childElements.forEach(function(el) {
+    item.appendChild(el)
+  })
+
+  return item
+}
+
 const handleSubmit = function(ev) {
   ev.preventDefault()
 
   const f = ev.target
-  const spellName = f.spellName.value
-  const level = f.level.value
+
+  const spell = {
+    name: f.spellName.value,
+    level: f.level.value,
+  }
+
+  const item = renderItem(spell)
 
   const list = document.querySelector('#spells')
-
-  const nameSpan = renderProperty('spellName', spellName)
-  const levelSpan = renderProperty('level', level)
-
-  const item = document.createElement('li')
-  item.classList.add('spell')
-  item.appendChild(nameSpan)
-  item.appendChild(levelSpan)
-
   list.appendChild(item)
 
   f.reset()
